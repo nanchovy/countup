a465c9660802c6855ceb557e40ddcd34edb5bde4
naoyukinagayasu <cyouan927@gmail.com>
2019-08-27 17:47:25 +0900

    changed indent to 2 spaces
    インデントをスペース2つに変更

--- src/components/ScoreBoard.vue
+++ src/components/ScoreBoard.vue
@@ -1,58 +1,56 @@
 <template>
-    <div>
-        <v-container>
-            <v-layout>
-                <div class="score_display">
-                    {{ history.reduce((sum, dart) => sum + dart.score, 0) }}
-                </div>
+  <div>
+    <v-container>
+      <v-layout>
+        <div class="score_display">{{ history.reduce((sum, dart) => sum + dart.score, 0) }}</div>
         <v-btn color="warning" @click="this.deleteLastHistory">undo</v-btn>
-                <v-btn color="error" @click="resetHistory">RESET</v-btn>
-            </v-layout>
-            <v-layout>
+        <v-btn color="error" @click="resetHistory">RESET</v-btn>
+      </v-layout>
+      <v-layout>
         <v-simple-table dense>
-            <thead>
+          <thead>
             <tr>
-                <th class="text-left">投目</th>
-                <th class="text-left">点数</th>
+              <th class="text-left">投目</th>
+              <th class="text-left">点数</th>
             </tr>
-            </thead>
-            <tbody>
+          </thead>
+          <tbody>
             <tr v-for="dart in history" v-bind:key="dart.order">
-                <td>{{ dart.order }}</td>
-                <td>{{ dart.score }}</td>
+              <td>{{ dart.order }}</td>
+              <td>{{ dart.score }}</td>
             </tr>
-            </tbody>
+          </tbody>
         </v-simple-table>
-            </v-layout>
-        </v-container>
-    </div>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
-    export default {
-        data: () => ({
-            totalScore: 0
-        }),
-        props: {
-            history: {
-                type: Array,
-                required: true
-            }
-        },
-        methods: {
-            deleteLastHistory () {
-                this.$emit('deleteLastHistory')
-            },
-            resetHistory () {
-                this.$emit('resetHistory')
-            }
-        }
+export default {
+  data: () => ({
+    totalScore: 0
+  }),
+  props: {
+    history: {
+      type: Array,
+      required: true
     }
+  },
+  methods: {
+    deleteLastHistory() {
+      this.$emit("deleteLastHistory");
+    },
+    resetHistory() {
+      this.$emit("resetHistory");
+    }
+  }
+};
 </script>
 
 <style>
-    .score_display {
-        font-size: 10rem;
-    }
+.score_display {
+  font-size: 10rem;
+}
 </style>
 
