af169889bb07075d3527079311a0f31707587e18
naoyukinagayasu <cyouan927@gmail.com>
2019-09-07 23:57:20 +0900

    feat: install VueRouter
    VueRouterを追加
    componentのimportに@を使わないとエラーが出る模様

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+<v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>ienage.com</span>
+        <!-- <span class="font-weight-light">COUNT UP</span> -->
+      </v-toolbar-title>
+    </v-app-bar>
+    <v-content>
+    <h1><router-link to="/countup">COUNTUP</router-link></h1>
+    <h1>SHOOTOUT(近日公開)</h1>
+    </v-content>
+  </v-app>
+
+</template>
